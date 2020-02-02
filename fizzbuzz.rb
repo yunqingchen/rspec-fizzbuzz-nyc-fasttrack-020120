1c204999a1e5954ae686a610e2e54ec8711ebd20
Clark Chen <yyunqingchenn@gmail.com>
2020-02-02 19:21:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@
 
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0 
-     "Fizz"
+     "FizzBuzz"
   elsif num % 5 == 0 
      "Buzz"
-  elsif num % 3 == 0 && num % 5 == 0
+  elsif num % 3 == 0
      "FizzBuzz"
   end 
 end 
